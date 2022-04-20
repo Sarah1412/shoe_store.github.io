79292e521fa83ddcd59c589dbe5ab56b26475285
Sarah1412 <73131433+Sarah1412@users.noreply.github.com>
2022-04-20 22:49:03 +0530

    Add files via upload

---
+++ Shoe_store/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', evt => {
+  console.log('service worker installed');
+  });
+  // activate event
+  self.addEventListener('activate', evt => {
+  console.log('service worker activated');
+  });
+  // fetch event
+  self.addEventListener('fetch', evt => {
+  console.log('fetch event', evt);
+  });
